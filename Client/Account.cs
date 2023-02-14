2d1717db037ec9fd916d2c3a675510ab4391c748
Maxim Selivanov <aikonradish@yandex.ru>
2023-02-14 14:50:01 +0300

    Добавьте файлы проекта.

---
+++ Client/Account.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Client
+{
+    public class Account
+    {
+        public int id;
+        private string login;
+        private string password;
+        private void CheckingTheEnteredString(string forCheck, string fieldName)
+        {
+            if (string.IsNullOrWhiteSpace(forCheck))
+                throw new ArgumentException(fieldName);
+            if(forCheck.Contains(' '))
+                throw new ArgumentException(fieldName);
+            if(forCheck.Length < 8)
+                throw new ArgumentException(fieldName);
+        }
+        public string Login
+        {
+            get => login;
+            set
+            {
+                CheckingTheEnteredString(value, nameof(Login));
+                login = value;
+            }
+        }
+        public string Password
+        { 
+            get => password;
+            set
+            {
+                CheckingTheEnteredString(value, nameof(Password));
+                password = value;
+            }
+        }
+        public Account(int id, string login, string password)
+        {
+            Login = login;
+            Password = password;
+            this.id = id;
+        }
+    }
+}
---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ApplicationContext db = new ApplicationContext();
+        List<Account> accounts;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            db.Database.EnsureCreated();
+            db.Accounts.Load();
+            DataContext = db.Accounts.Local.ToObservableCollection();
+
+            accounts = db.Accounts.ToList();
+            DbOutputTextBox.Text = PrintAccounts(accounts);
+        }
+
+        private void EnterButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                AddAccount();
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show($"Поле {ex.Message} заполнено неверно");
+            }
+        }
+        private void AddAccount()
+        {
+            Account account = RegisterNewAccount();
+            db.Accounts.Add(account);
+            db.SaveChanges();
+
+            Log.Text += $"id: {account.id}, login: {account.Login}";
+        }
+        public Account RegisterNewAccount()
+        {
+            string login = LoginTextBox.Text;
+            string password = PasswordTextBox.Password;
+            int id = accounts.Count + 1;
+            var account = new Account(id, login, password);
+            return account;
+        }
+        public string PrintAccounts(List<Account> accounts)
+        {
+            string resultString = "";
+            foreach(Account account in accounts)
+            {
+                resultString += $"id: {account.id}, login: {account.Login}\n";
+            }
+            return resultString;
+        }
+    }
+}
