43dd99d21260455d4c8e12bf51043be055e87c01
Selivanov Maxim <95645162+MaximSelivanoff@users.noreply.github.com>
2023-03-09 16:24:04 +0300

    Добавил временные метки в логи

--- Server/core/ServerLogMessages.cs
+++ Server/core/ServerLogMessages.cs
@@ -1,20 +1,22 @@
-﻿namespace Server
+﻿using System;
+
+namespace Server
 {
     static internal class ServerLogMessages
     {
         public static string LoginRequestAccepted(string registratedLogin)
         {
-            string message = $"Принят запрос на аутентификацию пользователя {registratedLogin}";
+            string message = $"{DateTime.Now}\nПринят запрос на аутентификацию пользователя {registratedLogin}";
             return message;
         }
         public static string TimeStampHashSended(string timeStampHash)
         {
-            string message = $"Отправлен хэш временной метки: {timeStampHash}";
+            string message = $"{DateTime.Now}\nОтправлен хэш временной метки: {timeStampHash}";
             return message;
         }
         public static string PasswordRequestChecked(string login, string timeStampHash)
         {
-            string message = $"Принят для проверки хэш пароля от пользователя:\nЛогин:{login} \nХэш: {timeStampHash}";
+            string message = $"{DateTime.Now}\nПринят для проверки хэш пароля от пользователя:\nЛогин:{login} \nХэш: {timeStampHash}";
             return message;
         }
     }
